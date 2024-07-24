2a7a0ed26a9d087edbdc52598432449cfe99d975
Nitai Charan <nitaicharan@gmail.com>
2024-07-24 12:43:35 +0100

    fix(openapi): change to openapi spec to remove warning and errors

--- src/main/java/personalspring/infrastructure/delegators/SignupApiDelegateImpl.java
+++ src/main/java/personalspring/infrastructure/delegators/SignupApiDelegateImpl.java
@@ -1,15 +1,7 @@
 package personalspring.infrastructure.delegators;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import personalspring.infrastructure.api.SignupApiDelegate;
-import personalspring.infrastructure.dto.SignUp;
 
 @Service
-public class SignupApiDelegateImpl implements SignupApiDelegate {
-
-  @Override
-  public ResponseEntity<Void> postSignup(SignUp signUp) {
-    return SignupApiDelegate.super.postSignup(signUp);
-  }
-}
+public class SignupApiDelegateImpl implements SignupApiDelegate {}
